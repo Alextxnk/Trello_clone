e4727b704665a1b5ba6df6339c15f09a57050dbd
Alextxnk <alexsolov766@gmail.com>
2022-06-15 22:16:46 +0300

    Add dist

---
+++ dist/_nuxt/40737f5.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{597:function(n,t,e){"use strict";e.r(t);var c={asyncData:function(){$nuxt.$fire.auth.signOut()}},l=e(57),component=Object(l.a)(c,(function(){var n=this,t=n.$createElement;return(n._self._c||t)("h1",[n._v("Sign out")])}),[],!1,null,null,null);t.default=component.exports}}]);
